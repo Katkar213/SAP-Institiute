67032b99cf81d3b3d5878221bfa0495e84c2e6e0
Katkar213 <blackship213@gmail.com>
2024-03-06 11:25:42 +0530

    all

--- js/script.js
+++ js/script.js
@@ -6,12 +6,12 @@ buttons.forEach((button) => {
   button.addEventListener("click", () => {
    
     const answer = button.nextElementSibling;
-    answer.classList.toggle("answer")
-  if(answer.style.display==="block"){
-    answer.style.display="none";
-  }
-  else{
-    answer.style.display="block";
-  }
+    answer.classList.toggle("show");
+  // if(answer.style.display==="block"){
+  //   answer.style.display="none";
+  // }
+  // else{
+  //   answer.style.display="block";
+  // }
   });
 });
